46069090802b37a4641db4e734deb8283e3439f5
Karol Górnik <karolgornik1997@gmail.com>
2022-02-26 11:16:09 +0100

    pierwsze zajecia

--- test.py
+++ test.py
@@ -1,5 +1,26 @@
 import sys
+import math
 
 print(sys.version)
-a = 5
+a = 'isi\ngrupa niestacjonarna'
+print(a)
+print(type(a))
+b = 5
+print(b)
+c = (5.4)
+print(c)
+d = 3+2j
+print(d)
+print(type(d))
 
+#nazwa_zmiennej = wartosc
+
+e = 7
+f = 3
+print(e // f)   #ile razy f zmiesci sie w e
+print(2/3 ** f)   #potegowanie
+# g = 'wizualizacja danych'
+# h = ' grupa 1'  #spacja przed drugim tekstem zeby byl odstep
+#
+# print(g+h)
+print(math.pow(2/3, f))  #e = liczba potegowana, f = wykladnik
